2e348fbd666050ecae460d926198513b2ee93028
Maykel <jotasenator@gmail.com>
2023-11-07 23:19:12 -0500

    Work on bear store, computed hack

---
+++ src/stores/bears/bears.store.ts
@@ -0,0 +1,54 @@
+import { create } from 'zustand'
+
+type Bear = {
+    id: number;
+    name: string;
+}
+
+type BearState = {
+    blackBears: number;
+    polarBears: number;
+    pandaBears: number;
+
+    bears: Bear[];
+
+    computed: {
+        totalBears: number;
+    }
+
+    increaseBlackBears: (by: number) => void;
+    increasePandaBears: (by: number) => void;
+    increasePolarBears: (by: number) => void;
+
+    doNothing: () => void;
+    addBears: () => void;
+    clearBears: () => void;
+
+}
+
+
+
+export const useBearStore = create<BearState>()((set, get) => ({
+    blackBears: 10,
+    polarBears: 5,
+    pandaBears: 1,
+
+    bears: [{ id: 1, name: 'Oso No 1' }],
+
+    computed: {
+        get totalBears() {
+            return get().blackBears + get().pandaBears + get().polarBears + get().bears.length
+        }
+    },
+
+    increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
+    increasePolarBears: (by: number) => set((state) => ({ polarBears: state.polarBears + by })),
+    increasePandaBears: (by: number) => set((state) => ({ pandaBears: state.pandaBears + by })),
+
+    doNothing: () => set(state => ({ bears: [...state.bears] })),
+    addBears: () => set(state => ({
+        bears: [...state.bears, { id: state.bears.length + 1, name: `Oso No${state.bears.length + 1}` }]
+    })),
+    clearBears: () => set(({ bears: [] })),
+}))
+
