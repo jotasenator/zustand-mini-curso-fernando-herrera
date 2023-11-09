ddfffc20b942dc0e68a4dbad29bfad7948222a25
Maykel <jotasenator@gmail.com>
2023-11-09 01:21:14 -0500

    Implement firebase storage

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -1,6 +1,7 @@
 import { StateCreator, create } from 'zustand'
 import { persist } from 'zustand/middleware';
-import { customSessionStorage } from '../storages/session-storage.storage';
+// import { customSessionStorage } from '../storages/session.storage';
+import { firebaseStorage } from '../storages/firebase.storage';
 
 type PersonState = {
     firstName: string;
@@ -28,6 +29,6 @@ export const usePersonStore = create<PersonState & Actions>()(
         storeAPI
         , {
             name: 'person-storage',
-            storage: customSessionStorage
+            storage: firebaseStorage
         })
 )
