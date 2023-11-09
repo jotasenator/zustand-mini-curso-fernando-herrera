d766117b6e00b447111914f9f70ce7de3646cd74
Maykel <jotasenator@gmail.com>
2023-11-08 23:45:10 -0500

    Use persist middleware

---
+++ src/stores/person/person.store.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware';
+
+type PersonState = {
+    firstName: string;
+    lastName: string;
+}
+
+type Actions = {
+    setFirstName: (value: string) => void;
+    setLastName: (value: string) => void;
+}
+
+export const usePersonStore = create<PersonState & Actions>()(
+    persist(
+        (set) => ({
+
+            firstName: '',
+            lastName: '',
+            setFirstName: (value: string) => set(state => ({ firstName: value })),
+            setLastName: (value: string) => set(state => ({ lastName: value })),
+
+        }), { name: 'person-storage' })
+)
